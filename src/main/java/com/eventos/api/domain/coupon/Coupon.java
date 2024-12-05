a7945c380b701d1dddbd6f62f1769e355a121a47
Natalia Taira <nataliagtaira@gmail.com>
2024-12-05 14:02:21 -0300

    feat: creating entities

---
+++ src/main/java/com/eventos/api/domain/coupon/Coupon.java
@@ -0,0 +1,31 @@
+package com.eventos.api.domain.coupon;
+
+import com.eventos.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "coupon")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date expiryDate;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
